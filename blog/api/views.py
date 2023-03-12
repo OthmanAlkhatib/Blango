b712550e17cbb121fc0149ecabb2ae60daaf8aed
Othman ALKhateeb <hamodyothman05@gmail.com>
2023-03-12 18:09:36 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,12 +2,14 @@ from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 # See views.pdf file, slide (14,15,16) to understand what's happening behind the scenes.
 # from rest_framework.authentication import SessionAuthentication
 
 class PostList(generics.ListCreateAPIView):
     # authentication_classes = [SessionAuthentication]
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
