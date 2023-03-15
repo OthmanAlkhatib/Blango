eac7b98955271626b099c18b5900ffa1b34ee5e9
Othman ALKhateeb <hamodyothman05@gmail.com>
2023-03-15 07:14:35 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostList, PostDetail, UserDetail
+# from blog.api.view import PostList, PostDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 from django.urls import path, include
 from rest_framework.authtoken import views
 
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('tags', TagViewSet)
+router.register("posts", PostViewSet)
+
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail")
 ]
 
@@ -15,4 +21,5 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("", include(router.urls)),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,26 +1,51 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from blango_auth.models import User
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
-from blog.models import Post
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
+from blog.models import Post, Tag
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
-# See Course2, Week2, views.pdf file, slide (14,15,16) to understand what's happening behind the scenes.
-# from rest_framework.authentication import SessionAuthentication
+# See Course2, Week2, views.pdf file, slide (14,15,16) to understand what's happening behind the scenes of ListCreateAPIView.
 
-class PostList(generics.ListCreateAPIView):
-    # authentication_classes = [SessionAuthentication]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostList(generics.ListCreateAPIView):
+#    # from rest_framework.authentication import SessionAuthentication
+#    # authentication_classes = [SessionAuthentication]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+#     queryset = Post.objects.all()
+#     serializer_class = PostDetailSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+# This Class Replaced Both PostList and PostDetail Views.
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer
 
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
+    
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(methods=['get'], detail=True, name="Posts with specific Tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        post_serializer = PostSerializer(
+            tag.posts, many=True, context={"request": request}
+        )
+        return Response(post_serializer.data)
